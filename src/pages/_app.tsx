1f5eabad5bf3c6534a20818e9b3ecb4cbb27212d
Pedro Cleis <pedrokleiz@gmail.com>
2021-08-16 10:22:44 -0400

    error in page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+  <>
+    <Header />
+    return <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
